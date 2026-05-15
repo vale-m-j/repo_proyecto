cf02b4a7c9bfd9f6eaa3db483e1769102289ae1c
Valentina Fernandez <Valentina.fernandezb24@gmail.com>
2026-05-15 21:15:09 +0000

    PROY-2: Agregar análisis de ventas

---
+++ scripts/analisis.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+
+ventas = pd.DataFrame({
+    'producto':['Lapiz','Cuaderno','Lapiz','Goma'],
+    'cantidad':[2,1,3,2],
+    'precio':[100,500,100,50]
+})
+
+ventas['total']=ventas['cantidad']*ventas['precio']
+
+print("Ventas totales:")
+print(ventas['total'].sum())
+
+print("Productos vendidos:")
+print(ventas.groupby('producto')['cantidad'].sum())
